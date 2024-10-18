f9d600cdfb84a7b36adbe59fd3551463e43877e1
Angel Yaguno <angel.yaguno@tecsup.edu.pe>
2024-10-18 17:03:42 -0500

    rutas agregadas en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from "./pages/Home/Home";
 import Header from "./layouts/header/Header";
+import Login from "./pages/Login/Login";
+import NotFound from "./pages/Not Found/NotFound";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home/>,
+    errorElement: <NotFound/>
+  },
+  {
+    path: '/login',
+    element: <Login/>
+  }
+]);
 
 export default function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
+      <RouterProvider router={router}/>
     </div>
   );
 }
