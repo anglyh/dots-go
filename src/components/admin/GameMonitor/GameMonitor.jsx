b9c97fae32f2ef932e3b8018ad49050851ec9aa2
Your Name <you@example.com>
2024-11-08 15:34:16 -0500

    Estilos

--- src/components/admin/GameMonitor/GameMonitor.jsx
+++ src/components/admin/GameMonitor/GameMonitor.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
-import styles from "./GameMonitor.module.css"
+import styles from "./GameMonitor.module.css";
 
-export default function GameMonitor ({ nombre, tiempo, codigo, onIniciarJuego }) {
+export default function GameMonitor({ nombre, tiempo, codigo, onIniciarJuego }) {
   return (
-    <div className={styles.gameMonitorContainer}>
-      <h2>¡Juego Creado!</h2>
-      <div className={styles.gameInfo}>
-        <p><strong>Nombre:</strong> {nombre}</p>
-        <p><strong>Tiempo:</strong> {tiempo} minutos</p>
+    <div>
+      {/* Contenedor de información del juego en la parte superior ocupando todo el ancho */}
+      <div className={styles.gameMonitorContainer}>
+        <h2>¡Juego Creado!</h2>
+        <div className={styles.gameInfo}>
+          <p><strong>Nombre:</strong> {nombre}</p>
+          <p><strong>Tiempo:</strong> {tiempo} minutos</p>
+        </div>
+        <div className={styles.gameCode}>
+          <p>Código de Juego</p>
+          <span>{codigo}</span>
+        </div>
+        <button className={styles.buttonForm} onClick={onIniciarJuego}>Iniciar Juego</button>
       </div>
-      <div className={styles.gameCode}>
-        <p>Código de Juego</p>
-        <span>{codigo}</span>
+
+      {/* Nueva sección de alumnos conectados ocupando toda la pantalla */}
+      <div className={styles.connectedStudentsContainer}>
+        <h3>Usuarios conectados</h3>
+        <div className={styles.studentsList}>
+          {Array.from({ length: 30 }).map((_, index) => (
+            <div key={index} className={styles.student}></div>
+          ))}
+        </div>
       </div>
-      <button className={styles.buttonForm} onClick={onIniciarJuego}>Iniciar Juego</button>
     </div>
   );
-};
-
+}
