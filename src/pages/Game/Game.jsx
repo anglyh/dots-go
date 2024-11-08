459171dcb5d5018b647b139487ab0a95d58c4e62
Saraicht <sarai.choqquepata@tecsup.edu.pe>
2024-11-08 15:37:46 -0500

    loginestilos

--- src/pages/Game/Game.jsx
+++ src/pages/Game/Game.jsx
@@ -9,7 +9,7 @@ import Button from "../../components/common/Button/Button"
 export default function Game() {
   const COLORS = ["white", "red", "black", "yellow", "blue", "green", "orange"];
   const NUMBERS = [
-    1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 2.1, 2.2, 2.3, 3, 4.1, 4.2, 4.3, 5.1, 5.2,
+    1, 2.1, 2.2, 2.3, 3, 4.1, 4.2, 4.3, 5.1, 5.2,
     6.1, 6.2, 7, 8, 9,
   ];
 
